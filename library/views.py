32c1936450c8611962cb3c1e5304e11aa87ce28e
xuyong <xuyong@yunrong.cn>
2018-12-25 14:54:43 +0800

    test

--- library/views.py
+++ library/views.py
@@ -1,4 +1,4 @@
-#auther by xpokers
+
 from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from    django.utils.decorators import method_decorator
